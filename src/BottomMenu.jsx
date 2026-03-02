20195aebfb81214852ef92aa483705382c4211da
raissaduarteb <raissaduarteb@gmail.com>
2026-03-02 16:03:34 -0300

    bottommenu

--- src/BottomMenu.jsx
+++ src/BottomMenu.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { useState } from "react";
 
 const BottomMenu = () => {
-  return <div></div>;
+  const [active, setActive] = useState("home");
+
+  return (
+    <div className="bottomMenu">
+      <button
+        className={`menu-item ${active === "home" ? "active" : ""}`}
+        onClick={() => setActive("home")}
+      >
+        <object
+          data="imgs/casinha.svg"
+          width="25px"
+          height="25px"
+          className="icon"
+        />{" "}
+        <span className="label">Home</span>
+      </button>
+      <button
+        className={`menu-item ${active === "selecao" ? "active" : ""}`}
+        onClick={() => setActive("selecao")}
+      >
+        <object
+          data="imgs/selecao.svg"
+          width="25px"
+          height="25px"
+          className="icon"
+        />{" "}
+        <span className="label">Seleção</span>
+      </button>
+      <button
+        className={`menu-item ${active === "ajustes" ? "active" : ""}`}
+        onClick={() => setActive("ajustes")}
+      >
+        <object
+          data="imgs/ajustes.svg"
+          width="25px"
+          height="25px"
+          className="icon"
+        />{" "}
+        <span className="label">Ajustes</span>
+      </button>
+    </div>
+  );
 };
 
 export default BottomMenu;
