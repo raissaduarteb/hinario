9d6b814ae83ac105d546fb94dc7509f67ab34de0
raissaduarteb <raissaduarteb@gmail.com>
2026-03-03 11:28:42 -0300

    comitando

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -2,8 +2,10 @@ import ClipLoader from "react-spinners/ClipLoader";
 
 function Loading() {
   return (
-    <div style={{ display: "flex", justifyContent: "center", padding: 20 }}>
-      <ClipLoader color="#4f46e5" size={50} />
+    <div
+      style={{ display: "flex", justifyContent: "center", marginTop: "100px" }}
+    >
+      <ClipLoader color="rgba(233, 78, 26, 1)" size={50} />
     </div>
   );
 }
