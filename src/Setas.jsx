c140fe2efba53e747a7f25257314bf7b24341590
raissaduarteb <raissaduarteb@gmail.com>
2026-03-19 10:17:45 -0300

    setas

--- src/Setas.jsx
+++ src/Setas.jsx
@@ -2,10 +2,10 @@ const Setas = () => {
   return (
     <div className="setas">
       <button type="button" className="seta seta-anterior">
-        <img src="/imgs/setahino.svg" width={"30%"}></img>
+        <img src="/imgs/setahino.svg" width={"30%"} alt="seta"></img>
       </button>
       <button type="button" className="seta seta-proximo">
-        <img src="/imgs/setahino.svg" width={"30%"}></img>
+        <img src="/imgs/setahino.svg" width={"30%"} alt="seta"></img>
       </button>
     </div>
   );
