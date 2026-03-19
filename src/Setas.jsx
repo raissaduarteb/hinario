1709022fc014739744654b42c11130a0b35c32b6
raissaduarteb <raissaduarteb@gmail.com>
2026-03-19 10:21:58 -0300

    setas

--- src/Setas.jsx
+++ src/Setas.jsx
@@ -1,11 +1,12 @@
+import SetasIcon from "../imgs/setahino.svg";
 const Setas = () => {
   return (
     <div className="setas">
       <button type="button" className="seta seta-anterior">
-        <img src="/imgs/setahino.svg" width={"30%"} alt="seta"></img>
+        <img src={SetasIcon} width={"30%"} alt="seta"></img>
       </button>
       <button type="button" className="seta seta-proximo">
-        <img src="/imgs/setahino.svg" width={"30%"} alt="seta"></img>
+        <img src={SetasIcon} width={"30%"} alt="seta"></img>
       </button>
     </div>
   );
